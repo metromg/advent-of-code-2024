02aa8273b73c6832095f2b026385f4277992b3c8
Michael Günter <zyborglp@outlook.com>
2024-12-04 23:02:29 +0100

    cleanup

--- 04/04.fsx
+++ 04/04.fsx
@@ -14,47 +14,44 @@ let isInBounds r c = r >= 0 && r < rows && c >= 0 && c < cols
 
 // Part 1
 let part1 =
-    let directions =
-        [ 
-            (0, 1)   // Right
-            (0, -1)  // Left
-            (1, 0)   // Down
-            (-1, 0)  // Up
-            (1, 1)   // Diagonal down-right
-            (1, -1)  // Diagonal down-left
-            (-1, 1)  // Diagonal up-right
-            (-1, -1) // Diagonal up-left
-        ]
-
-    let matchesAtPositionInDirection (grid: char array2d) row col direction (word: string) =
+    let matchesXmasAtPositionInDirection row col direction =
+        let word = "XMAS"
         let (rowStep, colStep) = direction
-        let wordLength = word.Length
 
         let rec check index r c =
-            if index = wordLength then true
+            if index = word.Length then true
             elif not (isInBounds r c) || grid.[r, c] <> word.[index] then false
             else check (index + 1) (r + rowStep) (c + colStep)
         
         check 0 row col
 
-    let findAllInstances grid word =
-        let rows = Array2D.length1 grid
-        let cols = Array2D.length2 grid
+    let allInstances =
+        let directions =
+            [ 
+                (0, 1)   // Right
+                (0, -1)  // Left
+                (1, 0)   // Down
+                (-1, 0)  // Up
+                (1, 1)   // Diagonal down-right
+                (1, -1)  // Diagonal down-left
+                (-1, 1)  // Diagonal up-right
+                (-1, -1) // Diagonal up-left
+            ]
 
         [ for row in 0 .. rows - 1 do
             for col in 0 .. cols - 1 do
                 for direction in directions do
-                    if matchesAtPositionInDirection grid row col direction word then
+                    if matchesXmasAtPositionInDirection row col direction then
                         yield (row, col, direction) ]
 
-    let count = findAllInstances grid "XMAS" |> Seq.length
+    let count = allInstances |> Seq.length
     printfn "Number of XMAS: %i" count
 
 part1
 
 // Part 2
 let part2 =
-    let matchesXPattern (grid: char array2d) row col =
+    let matchesXPattern row col =
         let positions =
             [
                 (row - 1, col - 1) // Top-left
@@ -69,16 +66,13 @@ let part2 =
             chars = ['M'; 'M'; 'S'; 'S'; 'A'] || chars = ['M'; 'S'; 'M'; 'S'; 'A'] || chars = ['S'; 'M'; 'S'; 'M'; 'A'] || chars = ['S'; 'S'; 'M'; 'M'; 'A']
         else false
 
-    let findAllInstances grid =
-        let rows = Array2D.length1 grid
-        let cols = Array2D.length2 grid
-        
+    let allInstances =
         [ for row in 0 .. rows - 1 do
             for col in 0 .. cols - 1 do
-                if matchesXPattern grid row col then
+                if matchesXPattern row col then
                     yield (row, col) ]
 
-    let count = findAllInstances grid |> Seq.length
+    let count = allInstances |> Seq.length
     printfn "Number of X-Pattern: %i" count
 
 part2
