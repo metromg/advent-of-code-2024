ebd4a69f7963f453516c3d449d17892ff6f72bed
Michael Günter <zyborglp@outlook.com>
2024-12-15 19:06:22 +0100

    black magic

--- 15/15.fsx
+++ 15/15.fsx
@@ -47,6 +47,13 @@ let parseMoves (movesInput: string) =
         | _ -> failwith "Failed to parse moves"
     )
 
+type MoveResult =
+    | Moved of ((int * int) * Map<(int * int), MapElement>)
+    | Blocked of ((int * int) * Map<(int * int), MapElement>)
+
+let toTuple =
+    function Moved (newPos, newMap) | Blocked (newPos, newMap) -> (newPos, newMap)
+
 let rec move pos dir map =
     let step dir (x, y) =
         match dir with
@@ -55,54 +62,53 @@ let rec move pos dir map =
         | Left -> (x - 1, y)
         | Right -> (x + 1, y)
 
-    let attemptMove prevPosOfPrevMove newPosOfPrevMove fallback next =
-        if prevPosOfPrevMove <> newPosOfPrevMove then next()
-        else fallback
-
-    let handleBoxMovement boxPos map fallback next =
-        let (newBoxPos, updatedMap) = map |> move boxPos dir
-        attemptMove boxPos newBoxPos fallback (fun () -> next updatedMap)
-
-    let handleConnectedBoxMovement boxPos boxElem map fallback next =
-        let getConnectedBoxPos (x, y) = function
-            | BoxLeft -> (x + 1, y)
-            | BoxRight -> (x - 1, y)
-            | _ -> failwith "Expected BoxLeft or BoxRight"
-
-        let connectedBoxPos = getConnectedBoxPos boxPos boxElem
-        handleBoxMovement connectedBoxPos map fallback (fun updatedMap ->
-            handleBoxMovement boxPos updatedMap fallback next
-        )
-
-    let moveElement currentPos newPos map =
-        let mapElement = map |> Map.find currentPos
-        let updatedMap =
-            map
-            |> Map.remove currentPos
-            |> Map.add newPos mapElement
-        (newPos, updatedMap)
+    let applyWhenNotBlocked movers =
+        movers
+        |> List.fold (fun currentResult mover ->
+            match currentResult with
+            | Moved (_, currentMap) -> mover currentMap
+            | Blocked _ -> Blocked (pos, map)
+        ) (Moved (pos, map))
 
     let posInDir = pos |> step dir
-    let mapElementInDir = map |> Map.tryFind posInDir
+    let mapElemInDir = map |> Map.tryFind posInDir
 
-    match mapElementInDir with
+    match mapElemInDir with
     | Some (Box) ->
-        handleBoxMovement posInDir map (pos, map) (fun updatedMap ->
-            updatedMap |> move pos dir
-        )
-    | Some (BoxLeft | BoxRight as connectedBox) ->
-        handleConnectedBoxMovement posInDir connectedBox map (pos, map) (fun updatedMap ->
-            updatedMap |> move pos dir
-        )
+        applyWhenNotBlocked [
+            (fun map -> map |> move posInDir dir) ;
+            (fun map -> map |> move pos dir)
+        ]
+    | Some (BoxLeft | BoxRight as boxElem) ->
+        applyWhenNotBlocked [
+            (fun map ->
+                let connectedBoxPos (x, y) boxElem =
+                    match boxElem with
+                    | BoxLeft -> (x + 1, y)
+                    | BoxRight -> (x - 1, y)
+                    | _ -> failwith "Expected BoxLeft or BoxRight"
+
+                map |> move (connectedBoxPos posInDir boxElem) dir
+            ) ;
+            (fun map -> map |> move posInDir dir) ;
+            (fun map -> map |> move pos dir)
+        ]
     | Some _ ->
-        (pos, map)
+        Blocked (pos, map)
     | None ->
-        map |> moveElement pos posInDir
+        let mapElement = map |> Map.find pos
+        let updatedMap =
+            map
+            |> Map.remove pos
+            |> Map.add posInDir mapElement
+        Moved (posInDir, updatedMap)
 
 let processMoves moves startPos map =
     moves
     |> Array.fold (fun (pos, map) dir ->
-        map |> move pos dir
+        map
+        |> move pos dir
+        |> toTuple
     ) (startPos, map)
     |> snd
 
